53d439cf8dafa53dd077f5852029538a632437c7
Rishav Raj <rishav.raj.mec23@itbhu.ac.in>
2026-09-06 12:28:45 +0530

    login working

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Lock, Mail, User2Icon } from 'lucide-react'
+import { useDispatch } from 'react-redux'
+import { login } from '../app/features/authSlice'
+import toast from 'react-hot-toast'
+import api from '../configs/api'
 
 const Login = () => {
 
+  const dispatch= useDispatch()
   const query = new URLSearchParams(window.location.search)
   const urlState = query.get('state')
   const [state, setState] = React.useState(urlState || "login")
@@ -15,7 +20,14 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-
+    try {
+      const {data} = await api.post(`/api/users/${state}`, formData)
+      dispatch(login(data))
+      localStorage.setItem('token', data.token)
+      toast.success(data.message)
+    } catch (error) {
+      toast(error?.response?.data?.message || error.message)
+    }
   }
 
   const handleChange = (e) => {
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,7 +5,7 @@ import protect from "../middlewares/authMiddleware.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/regsiter', registerUser);
+userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/data',protect, getUserById);
 userRouter.get('/resumes', protect, getUserResumes);
